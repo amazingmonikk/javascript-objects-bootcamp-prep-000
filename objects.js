5f492697fad5c53e559454ca0f5b6d1ba1963bd2
Monika Tadross <monikk.tadross@gmail.com>
2019-02-08 01:02:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist['songTitle'] = 'artistName';
+    playlist['songTitle'] = 'artistName';
   //return playlist;
 }
 
